be00d922372bd2d270dfeb1004ce4bbf773b6486
FabioGavinelli <fabiogavinelli@gmail.com>
2026-01-30 18:28:29 +0100

    Add playGame function
    playGame handles the main game loop making the player play 5 round of rock, paper, scissor

--- Script/rock-paper-scissor.js
+++ Script/rock-paper-scissor.js
@@ -22,36 +22,46 @@ function getHumanChoice() {
     return choice;
 }
 
-function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.toLowerCase();
-    computerChoice = computerChoice.toLowerCase();
-    
-    console.log(`Human Choice: ${humanChoice} - Computer ${computerChoice}`);
 
-    if (humanChoice === computerChoice) {
-        console.log("IT'S A DRAW!");
-    } else if (humanChoice === "scissor" && computerChoice === "paper") {
-        humanScore++;
-        logRoundWinner("Human");
-    } else if (humanChoice === "paper" && computerChoice === "rock") {
-        humanScore++;
-        logRoundWinner("Human");
-    } else if (humanChoice === "rock" && computerChoice === "scissor") {
-        humanScore++;
-        logRoundWinner("Human");
-    } else {
-        computerScore++;
-        logRoundWinner("Computer");
+function playGame() {
+    function playRound(humanChoice, computerChoice) {
+        humanChoice = humanChoice.toLowerCase();
+        computerChoice = computerChoice.toLowerCase();
+        
+        console.log(`Human Choice: ${humanChoice} - Computer ${computerChoice}`);
+
+        if (humanChoice === computerChoice) {
+            console.log("IT'S A DRAW!");
+        } else if (humanChoice === "scissor" && computerChoice === "paper") {
+            humanScore++;
+            logRoundWinner("Human");
+        } else if (humanChoice === "paper" && computerChoice === "rock") {
+            humanScore++;
+            logRoundWinner("Human");
+        } else if (humanChoice === "rock" && computerChoice === "scissor") {
+            humanScore++;
+            logRoundWinner("Human");
+        } else {
+            computerScore++;
+            logRoundWinner("Computer");
+        }
+    }
+
+    function logRoundWinner(winner){
+        console.log(`${winner} win this round!
+    SCORE
+    H: ${humanScore}
+    C: ${computerScore}`);
+    }
+
+    console.log("THE GAME BEGINS! GOOD LUCK TO EVERYONE");
+
+    for(let i = 0; i <= 5; i++){
+        let cc = getComputedChoice();
+        let hc = getHumanChoice();
+        playRound(hc, cc);
     }
 }
 
-function logRoundWinner(winner){
-    console.log(`${winner} win this round!
-SCORE
-H: ${humanScore}
-C: ${computerScore}`);
-}
+playGame();
 
-let cc = getComputedChoice();
-let hc = getHumanChoice();
-playRound(hc, cc);
